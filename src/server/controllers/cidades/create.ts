ad0fc705963f51e5ab374957e9b1004c3cfe1b66
EngMateusCardoso <engmateuscardosooliveira@gmail.com>
2023-08-18 17:52:00 -0300

    fix tests

--- src/server/controllers/cidades/create.ts
+++ src/server/controllers/cidades/create.ts
@@ -25,5 +25,5 @@ export const create = async (req: Request<{}, {}, IBodyProps>, res: Response) =>
   if (result instanceof Error) {
     return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ errors: {default: result.message} })
   }
-  return res.status(StatusCodes.CREATED).json(result)
+  return res.status(StatusCodes.CREATED).json({"id": result})
 }
--- tests/jest.setup.ts
+++ tests/jest.setup.ts
@@ -1,5 +1,18 @@
 import supertest from "supertest";
 import server from "../src/server/server";
+import { Knex } from "../src/server/database/knex";
+
+// Run migrations before all tests
+// Executa as migrations antes de todos os testes
+beforeAll(async () => {
+  await Knex.migrate.latest();
+});
+
+// Run seeds before each test
+// Executa os seeds antes de cada teste
+afterAll(async () => {
+  await Knex.destroy();
+});
 
 // Setup the test server
 // Retorna uma instância do servidor
