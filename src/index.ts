713f458786b5312cd9c6a763fbdc77725f33ff29
EngMateusCardoso <engmateuscardosooliveira@gmail.com>
2023-08-19 01:16:33 -0300

    class 29 fix deploy prod

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,19 @@
+import { Knex } from './server/database/knex'
 import server from './server/server'
 
-// Ouvindo o servidor na porta de .env se for nula ouve a porta 3300
-server.listen(process.env.PORT || 3300, () => {
-  console.log(`Server listening on port ${process.env.PORT || 3300}`)
-})
+const startServer = () => {
+  // Ouvindo o servidor na porta de .env se for nula ouve a porta 3300
+  server.listen(process.env.PORT || 3300, () => {
+    console.log(`Server listening on port ${process.env.PORT || 3300}`)
+  })
+}
+
+if(process.env.IS_LOCALHOST !== 'true') {
+  // Executa as migrations e depois inicia o servidor
+  Knex.migrate.latest().then(() => {
+    startServer()
+  }).catch(console.log)
+} else {
+  // No localhost não é necessário executar as migrations toda vez que o servidor inicia
+  startServer()
+}
